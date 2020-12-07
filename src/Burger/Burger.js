4f44e6af9b56be9b01ebd55100be26d598512c73
Evgeniya <evgeniya@cobalt.io>
2020-12-07 11:56:56 +0100

    Create burger ingredients

--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import BurgerIngredients from './BurgerIngredients'
+import styles from './Burger.module.css'
+
+const burger = ({ingredients}) => {
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredients type='breadTop'/>
+      <BurgerIngredients type='cheese'/>
+      <BurgerIngredients type='meat'/>
+      <BurgerIngredients type='breadBottom'/>
+    </div>
+  )
+}
+
+export default burger
--- src/BurgerBuilder.js
+++ src/BurgerBuilder.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Burger from './Burger/Burger'
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 0,
+      cheese: 1,
+      meat: 2,
+    }
+  }
+
   render() {
     return (
       <>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </>
     )
